da86d2efe189543949e6dced6bb4578751f41117
Maria Luiza Ribeiro <163929189+MariaRuasCode@users.noreply.github.com>
2024-09-11 20:39:47 -0300

    Adicionando barras que movem na home

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Home from './pages/home';
 import Sets from './pages/sets';
 import Books from './pages/books';
 import QnA from './pages/qna';
-
+import ninjago_logo from './assets/images/ninjago_logo.png'
 function App() {
   return (
+    
     <Router>
+      
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
